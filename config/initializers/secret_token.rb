521b8dc28f63f5d3b92e2f15df20e8e479264206
Cedric Pimenta <cedricpim@gmail.com>
2012-11-13 15:52:34 +0000

    Trash

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-PlazrStore::Application.config.secret_token = '98318f9f72fdda2afac441434b57befec5f2d5c418669523ecfb46bfff3b5bf92c41fa373c730a34db550292f7092e473bc1b789b54e21556ee2c37e247f4962'
