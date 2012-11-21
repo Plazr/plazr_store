f932d3bd5f071815c425abc74b622e0dc4e7f823
Bruno Azevedo <azevedo.252@gmail.com>
2012-11-21 12:13:39 +0000

    Rspec defined as Integration tool and Test Framework

--- lib/plazr_store/engine.rb
+++ lib/plazr_store/engine.rb
@@ -1,6 +1,8 @@
 module PlazrStore
   class Engine < ::Rails::Engine
     isolate_namespace PlazrStore
+    config.generators.integration_tool :rspec
+    config.generators.test_framework :rspec
   end
 
   PZS=PlazrStore
