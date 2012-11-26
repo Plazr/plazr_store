3441228c9ea4b5138ce8ff8f5443534baae62d7e
Bruno Azevedo <azevedo.252@gmail.com>
2012-11-26 00:52:03 +0000

    changed rspec tests output format to 'documentation'; added a commented line to change the mocking setup of rspec to mocha

--- test/dummy/spec/spec_helper.rb
+++ test/dummy/spec/spec_helper.rb
@@ -18,12 +18,12 @@ Spork.prefork do
 
   RSpec.configure do |config|
     config.mock_with :rspec
+    # config.mock_with :mocha
     config.use_transactional_fixtures = true
     config.treat_symbols_as_metadata_keys_with_true_values = true
     config.filter_run focus: true
     config.run_all_when_everything_filtered = true
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
-    config.use_transactional_fixtures = true
+    # config.fixture_path = "#{::Rails.root}/spec/fixtures"
     config.infer_base_class_for_anonymous_controllers = false
     config.order = "random"
   end
