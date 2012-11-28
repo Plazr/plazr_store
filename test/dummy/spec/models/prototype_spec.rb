a3aa11e506dd62e4edb507ee743f70b62d4fce1c
Bruno Azevedo <azevedo.252@gmail.com>
2012-11-28 16:32:12 +0000

    fixed prototype_variant_property model; started specs for prototype

--- test/dummy/spec/models/prototype_spec.rb
+++ test/dummy/spec/models/prototype_spec.rb
@@ -9,8 +9,19 @@ describe PZS::Prototype, type: :model do
     it "has many products" do 
       FactoryGirl.create(:prototype).should have_many :products
     end
-    xit "has and belongs to many properties" do
-      should have_and_belong_to_many(:properties)
+
+    it "has many property_prototypes" do
+      FactoryGirl.create(:prototype).should have_many :property_prototypes
+    end
+    it "has many properties through property_prototypes" do
+      FactoryGirl.create(:prototype).should have_many(:properties).through(:property_prototypes)
+    end
+
+    it "has many prototype_variant_properties" do
+      FactoryGirl.create(:prototype).should have_many :prototype_variant_properties
+    end
+    it "has many variant_properties through prototype_variant_properties" do
+      FactoryGirl.create(:prototype).should have_many(:variant_properties).through(:prototype_variant_properties)
     end
   end
 
