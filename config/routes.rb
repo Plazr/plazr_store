b4a0a89c7f0716b185d5e12669ae16105972b9d9
Gabriel Poça <gabrielpoca@gmail.com>
2012-12-04 12:08:07 +0000

    routes for admin

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ PlazrStore::Engine.routes.draw do
 		get 'single' => 'pages#single'
 		get 'about' => 'pages#about'
 		root to: 'pages#index'
+		namespace :admin do
+			get 'index' => 'pages#index'
+		end
 	end
 
 	root to: 'mockup/pages#index'
