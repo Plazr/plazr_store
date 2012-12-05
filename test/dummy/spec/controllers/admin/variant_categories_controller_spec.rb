db73ac6f76392fd83e008b7278cf3f51f8c35455
Cedric Pimenta <cedricpim@gmail.com>
2012-12-05 00:56:47 +0000

    minor fixes to VariantCategoriesController spec (create and update methods)

--- test/dummy/spec/controllers/admin/variant_categories_controller_spec.rb
+++ test/dummy/spec/controllers/admin/variant_categories_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe PZS::Admin::VariantCategoriesController, :type => :controller do 
-  render_views
+  #render_views
 
   describe "GET #index" do
     it_behaves_like 'default admin index method', :variant_category
@@ -47,11 +47,88 @@ describe PZS::Admin::VariantCategoriesController, :type => :controller do
   end
 
   describe "POST #create" do
-    it_behaves_like 'default admin create method', :variant_category, PZS::VariantCategory
+    context "with valid attributes" do
+      it "saves the new variant_category in the database" do
+        expect{
+          post :create, :variant_category => FactoryGirl.attributes_for(:variant_category)
+        }.to change(PZS::VariantCategory, :count).by(1)
+      end
+      it "redirects to the :show template" do
+        post :create, :variant_category => FactoryGirl.attributes_for(:variant_category)
+        response.should redirect_to send("admin_variant_category_url".to_sym, PZS::VariantCategory.send(:last))
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new variant_category in the database" do
+        expect{
+          post :create, :variant_category => FactoryGirl.attributes_for("invalid_variant_category".to_sym)
+        }.to_not change(PZS::VariantCategory, :count)
+      end
+
+      it "assigns all variant_categories except the one to be edited to @variant_categories" do
+        m = FactoryGirl.create_list(:variant_category, 5)
+        n = FactoryGirl.create :variant_category
+        get :edit, id: n
+        PZS::VariantCategory.stub(:where).and_return(m)
+        assigns(:variant_categories).should eq(m)
+      end
+
+      it "re-renders the :new template" do
+        post :create, :variant_category => FactoryGirl.attributes_for("invalid_variant_category".to_sym)
+        response.should render_template :new
+      end
+    end
   end
 
   describe 'PUT #update' do
-    it_behaves_like 'default admin update method', :variant_category, :name
+    let(:existing_instance) {FactoryGirl.create(:variant_category)}
+
+    context "valid attributes" do
+      let(:new_instance) { FactoryGirl.attributes_for("variant_category_v2".to_sym)}
+
+      it "locates the requested variant_category" do
+        put :update, id: existing_instance, :variant_category => new_instance
+        assigns(:variant_category).should eq(existing_instance)      
+      end
+
+      it "changes variant_category's attributes" do
+        put :update, id: existing_instance, :variant_category => new_instance
+        existing_instance.reload # to check that our updates are actually persisted
+        existing_instance.send(:name).should eq(new_instance[:name])
+      end
+
+      it "redirects to the updated variant_category" do
+        put :update, id: existing_instance, :variant_category => new_instance
+        response.should redirect_to send("admin_variant_category_url".to_sym, existing_instance)
+      end
+    end
+
+    context "invalid attributes" do
+      it "locates the requested variant_category" do
+        put :update, id: existing_instance, :variant_category => FactoryGirl.attributes_for("invalid_variant_category".to_sym)
+        assigns(:variant_category).should eq(existing_instance)      
+      end
+
+      it "does not change variant_category's attributes" do
+        put :update, id: existing_instance, :variant_category => FactoryGirl.attributes_for("invalid_variant_category".to_sym)
+        existing_instance.reload # to check that our updates are actually persisted
+        existing_instance.send(:name).should_not be_nil
+      end
+      
+      it "assigns all variant_categories except the one being edited to @variant_categories" do
+        m = FactoryGirl.create_list(:variant_category, 5)
+        n = FactoryGirl.create :variant_category
+        get :edit, id: n
+        PZS::VariantCategory.stub(:where).and_return(m)
+        assigns(:variant_categories).should eq(m)
+      end
+
+      it "re-renders the edit method" do
+        put :update, id: existing_instance, :variant_category => FactoryGirl.attributes_for("invalid_variant_category".to_sym)
+        response.should render_template :edit
+      end
+    end
   end
 
   describe 'DELETE #destroy' do
