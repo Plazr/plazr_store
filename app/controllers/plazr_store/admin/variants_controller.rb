ebfb4da28830ab120baa477663c71268a0af3c90
Cedric Pimenta <cedricpim@gmail.com>
2012-12-12 18:21:27 +0000

    variant property values and variant categories working

--- app/controllers/plazr_store/admin/variants_controller.rb
+++ app/controllers/plazr_store/admin/variants_controller.rb
@@ -16,16 +16,19 @@ module PlazrStore
       if @variant.save
         redirect_to admin_product_variant_path(@product, @variant), :notice => 'Variant was successfully created.'
       else
+        build_relations_for_fields_for
         render :new
       end
     end
 
     def new
       @variant = Variant.new(visible: true)
+      build_relations_for_fields_for
     end
 
     def edit
       @variant = Variant.find params[:id]
+      build_relations_for_fields_for
     end
 
     def update
@@ -34,6 +37,7 @@ module PlazrStore
       if @variant.update_attributes(params[:variant])
         redirect_to admin_product_variant_path(@product, @variant), :notice => 'Variant was successfully updated.'
       else
+        build_relations_for_fields_for
         render :edit 
       end
     end
@@ -47,6 +51,14 @@ module PlazrStore
     protected
       def get_product
         @product = Product.find(params[:product_id])
+      end
+
+      # builds certain product relations so that fields_for can render properly
+      def build_relations_for_fields_for
+        # builds variant_categories that are not persisted so that fields_for can render them
+        @variant.get_unselected_variant_categories_and_order_by_name
+        # builds variant_properties that are not persisted so that fields_for can render them
+        @variant.get_unselected_variant_properties_and_order_by_name
       end
   end
 end
