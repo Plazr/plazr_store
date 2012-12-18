2bcdfeeb2922a7bf248998c525956e8ef644dd95
Pedro Costa <dev@pfac.info>
2012-12-18 15:12:13 +0000

    Removed bugged migration

--- db/migrate/20121114163121_add_page_id_and_variant_id_to_multimedia.rb
+++
@@ -1,9 +0,0 @@
-class AddPageIdAndVariantIdToMultimedia < ActiveRecord::Migration
-  def change
-    add_column :plazr_store_multimedia, :page_id, :integer
-    add_column :plazr_store_multimedia, :variant_id, :integer
-
-    add_index :plazr_store_multimedia, :page_id
-    add_index :plazr_store_multimedia, :variant_id
-  end
-end
