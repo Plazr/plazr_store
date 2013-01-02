c852cecbbacd94cc24990e4c7e080aed10d7dbac
Cedric Pimenta <cedricpim@gmail.com>
2013-01-02 15:14:16 +0000

    crud pages for admin done; integration with redactor still not done

--- test/dummy/spec/models/page_spec.rb
+++ test/dummy/spec/models/page_spec.rb
@@ -16,9 +16,15 @@ describe PZS::Page, :type => :model do
     it "requires the presence of a title" do
       FactoryGirl.create(:page).should validate_presence_of :title
     end
+    it "requires the presence of a slug" do
+      FactoryGirl.create(:page).should validate_presence_of :slug
+    end
     it "does not allow duplicate slug" do
       FactoryGirl.create(:page, slug: "page-x")
       FactoryGirl.build(:page, slug: "page-x").should_not be_valid
+    end
+    it "requires the presence of a content" do
+      FactoryGirl.create(:page).should validate_presence_of :content
     end
   end
 end
