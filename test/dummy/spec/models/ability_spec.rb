d5e6388fabcad13377b5bb03a819a4d864a65ad8
Bruno Azevedo <azevedo.252@gmail.com>
2013-01-05 17:12:01 +0000

    added more examples to ability_spec

--- test/dummy/spec/models/ability_spec.rb
+++ test/dummy/spec/models/ability_spec.rb
@@ -8,16 +8,35 @@ describe Ability do
         current_user = nil
         subject(:ability){ Ability.new(current_user) }
 
-        pending "Waiting for the logged out user to have a role"
         it_behaves_like 'unregistered role'
+        it "cannot access wishlist actions" do
+          should_not be_able_to(:access, :wishlist_actions)
+        end
+        it "cannot access cart actions" do
+          should_not be_able_to(:access, :cart_actions)
+        end
+        it "cannot access order actions" do
+          should_not be_able_to(:access, :orders_actions)
+        end
+
+        it "cannot manage nor admin the store" do
+          should_not be_able_to(:manage, :store)
+        end
       end
       context "when user is logged in" do
-        context "when user is client (role: 'user')" do
+        context "when user is client (role: 'user')", focus: true do
           current_user = FactoryGirl.create :user_with_user_role
           subject(:ability){ Ability.new(current_user) }
 
           it_behaves_like 'unregistered role'
           it_behaves_like 'user role', current_user
+          it "cannot show an order that doesn't belong to him" do
+            should_not be_able_to(:read, PZS::Order.new)
+          end
+
+          it "cannot manage nor admin the store" do
+            should_not be_able_to(:manage, :store)
+          end
         end
 
         context "when user is staff (role: 'staff')" do
@@ -27,6 +46,9 @@ describe Ability do
           it_behaves_like 'unregistered role'
           it_behaves_like 'user role', current_user
           it_behaves_like 'staff role'
+          it "cannot admin the store", focus: true do
+            should_not be_able_to(:admin, :store)
+          end
         end
 
         context "when user is admin (role: 'admin')" do
