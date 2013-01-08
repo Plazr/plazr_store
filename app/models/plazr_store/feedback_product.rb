9e8199c6e1d3d2889f35f26c552374438a77cdd2
Miguel Palhas <mpalhas@gmail.com>
2013-01-08 17:17:17 +0000

    added relation between feedback_product and order

--- app/models/plazr_store/feedback_product.rb
+++ app/models/plazr_store/feedback_product.rb
@@ -3,6 +3,7 @@ module PlazrStore
     ## Relations ##
     # belongs_to :user TODO metodo que chama api
     belongs_to :product
+    belongs_to :order
     
     ## Attributes ##
     attr_accessible :comment, :rating
