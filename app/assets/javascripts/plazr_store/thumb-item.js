987864c3ea0393e8028635e81bd90a8d8e9a28e8
Gabriel Poça <gabrielpoca@gmail.com>
2013-01-16 02:09:39 +0000

    featured done

--- app/assets/javascripts/plazr_store/thumb-item.js
+++ app/assets/javascripts/plazr_store/thumb-item.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-	var ThumbItem = function(elem) {
+	var ThumbItem = function(elem, span) {
 		this.elem = elem;
+		this.span = span;
 		this.update = function() {
-			spanWidth = $('.span3').width() - 30;
+			spanWidth = $(this.span).width() - 30;
 			$(this.elem).height(spanWidth);
 		}
 	}
-	thumbItem = new ThumbItem(".thumb-item-image");
+	thumbItem = new ThumbItem(".thumb-item-image", ".span3");
 	thumbItem.update();
+
+	// featured = new ThumbItem("#featured .featured-image", ".span5");
+	// featured.update();
 
 	$(window).resize(function() {
 		thumbItem.update();
