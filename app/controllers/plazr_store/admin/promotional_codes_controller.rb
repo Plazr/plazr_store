2e3f194865363aa4aff842dd8e67cad77aa9b8f2
Cedric Pimenta <cedricpim@gmail.com>
2013-01-16 19:17:53 +0000

    promotional codes crud

--- app/controllers/plazr_store/admin/promotional_codes_controller.rb
+++ app/controllers/plazr_store/admin/promotional_codes_controller.rb
@@ -1,7 +1,50 @@
 module PlazrStore
   class Admin::PromotionalCodesController < Admin::ApplicationController 
 
+    def show
+      @promotional_code = PromotionalCode.find(params[:id]) 
+    end
 
+    def index
+      @promotional_codes = PromotionalCode.order("starts_at DESC, expires_at ASC")
+    end
 
+    def new
+      @promotional_code = PromotionalCode.new
+      @discount_types = DiscountType.promotional_code_types
+    end
+
+    def create
+      @promotional_code = PromotionalCode.new params[:promotional_code]
+
+      if @promotional_code.save
+        redirect_to admin_promotional_code_path(@promotional_code), :notice => 'PromotionalCode was successfully created.' 
+      else
+        @discount_types = DiscountType.promotional_code_types
+        render :new
+      end
+    end
+
+    def edit
+      @promotional_code = PromotionalCode.find(params[:id])
+      @discount_types = DiscountType.promotional_code_types
+    end
+
+    def update
+      @promotional_code = PromotionalCode.find(params[:id])
+
+      if @promotional_code.update_attributes(params[:promotional_code])
+        redirect_to admin_promotional_code_path(@promotional_code), :notice => 'PromotionalCode was successfully updated.' 
+      else
+        @discount_types = DiscountType.promotional_code_types
+        render :edit
+      end
+    end
+
+    def destroy
+      @promotional_code = PromotionalCode.find(params[:id])
+      @promotional_code.destroy
+      redirect_to admin_promotional_codes_path
+    end
   end
 end
