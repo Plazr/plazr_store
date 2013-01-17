5c70495b6ac4080a4f2f2d6aeb968a1fd8496676
72MiguelGomes <jmbg.41@gmail.com>
2013-01-17 12:30:00 +0000

    new url and combobox done

--- app/assets/javascripts/plazr_store/search/search.js
+++ app/assets/javascripts/plazr_store/search/search.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
 
-	$("#filters button").click(getValues);
+	$("#filters a").click(getValues);
 
 	function getValues(){
 		var search = $('.clearfix #search').val();
 		var min_price = $('input[name="min_price"]').val();
 		var max_price = $('input[name="max_price"]').val();
 
+		var category = $('#cat option:selected').val();
 		var brand = $('#brand option:selected').val();
 		var price = $('#price option:selected').val();
 
 		var newURL = window.location.protocol + "://" + window.location.host + "/"
 
-		window.location = "/search?utf8=✓&search=" + search + "&min_price=" + min_price + "&max_price=" + max_price + "&brand=" + brand + "&price=" + price;
+		window.location = "/search?utf8=✓&search=" + search + "&cat=" + category + "&min_price=" + min_price + "&max_price=" + max_price + "&brand=" + brand + "&price=" + price;
 	}
 });
