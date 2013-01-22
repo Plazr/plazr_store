61013db3fd71184e09080f8ffbb9bf26e2700504
72MiguelGomes <jmbg.41@gmail.com>
2013-01-22 16:59:37 +0000

    product buttons working

--- app/helpers/plazr_store/wishlist_helper.rb
+++ app/helpers/plazr_store/wishlist_helper.rb
@@ -2,8 +2,8 @@ module PlazrStore
   module WishlistHelper
 
     def add_to_wishlist_form(variant_id)
-      form_tag wishlist_add_path(variant_id), method: :post do
-        submit_tag('Add to Wishlist')
+      form_tag wishlist_add_path(variant_id), :id => "add_wishlist_f",method: :post do
+      	
       end
     end
 
