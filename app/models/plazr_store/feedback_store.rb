57a40ac830f219daefd9fea03f604ff85059bcc7
Bruno Azevedo <azevedo.252@gmail.com>
2013-01-23 11:57:19 +0000

    removed feedback_store

--- app/models/plazr_store/feedback_store.rb
+++
@@ -1,9 +0,0 @@
-module PlazrStore
-  class FeedbackStore < ActiveRecord::Base
-    ## Relations ##
-    # belongs_to :user TODO metodo que chama api
-    
-    ## Attributes ##
-    attr_accessible :comment, :rating
-  end
-end
