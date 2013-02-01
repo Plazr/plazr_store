98903ea02a377e9f59e2b36f6e50bbf8d0998370
Miguel Palhas <mpalhas@gmail.com>
2013-02-01 11:00:02 +0000

    added app controller filters, originally from PZS::ApplicationController

--- app/controllers/plazr_store/application_controller.rb
+++ app/controllers/plazr_store/application_controller.rb
@@ -1,10 +1,10 @@
 module PlazrStore
   class ApplicationController < ActionController::Base
 
-  before_filter :get_categories
-  before_filter :get_pages
-  before_filter :get_banner
-  before_filter :get_logo
+  # before_filter :get_categories
+  # before_filter :get_pages
+  # before_filter :get_banner
+  # before_filter :get_logo
   before_filter :get_namespace
 
   rescue_from CanCan::AccessDenied do |exception|
@@ -24,21 +24,21 @@ module PlazrStore
     @current_ability ||= Ability.new(current_user, namespace)
   end
 
-  def get_pages
-    @pages = Page.all
-  end
+  # def get_pages
+  #   @pages = Page.all
+  # end
 
-  def get_categories
-    @categories = ProductCategory.all
-  end
+  # def get_categories
+  #   @categories = ProductCategory.all
+  # end
 
-  def get_banner
-    @banner = Multimedium.banner
-  end
+  # def get_banner
+  #   @banner = Multimedium.banner
+  # end
 
-  def get_logo
-    @logo = Multimedium.logo
-  end
+  # def get_logo
+  #   @logo = Multimedium.logo
+  # end
 
   def get_namespace
     @namespace = namespace
