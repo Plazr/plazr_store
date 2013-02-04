d09499ae7226e7a7f5924369c046b875d6a9dfe9
Cedric Pimenta <cedricpim@gmail.com>
2013-02-04 12:09:30 +0000

    translated discount_types

--- app/models/plazr_store/product_promotion.rb
+++ app/models/plazr_store/product_promotion.rb
@@ -12,7 +12,7 @@ module PlazrStore
 
     def one_active_promotion
       if self.product.promotions.where("(? BETWEEN starts_at AND expires_at) OR (? BETWEEN starts_at AND expires_at) OR (? >= starts_at AND expires_at IS NULL)", self.promotion.starts_at, self.promotion.expires_at, self.promotion.starts_at).count > 0
-        self.promotion.errors.add(:base, "#{self.product.name} already has a promotion in this period of time. Promotion not created")
+        promotion.errors.add(:base, "#{self.product.name} already has a promotion in this period of time. Promotion not created")
         #puts self.promotion.errors.inspect
         puts self.promotion
         false
