f7c71d54f43bda79a0cf1a03aa3a5386b18ce377
Gabriel Poça <gabrielpoca@gmail.com>
2013-02-14 00:41:54 +0000

    redactor removed

--- app/models/redactor_rails/asset.rb
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-class RedactorRails::Asset < ActiveRecord::Base
-  include RedactorRails::Orm::ActiveRecord::AssetBase
-  delegate :url, :current_path, :size, :content_type, :filename, :to => :data
-  validates_presence_of :data
-end
